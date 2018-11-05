fd5b501a96aff49f58c75762a34d4c0385e8ba03
bmontellano <bmontellano@gmail.com>
2018-11-04 18:58:27 -0800

    thats it for this project

--- app/index.js
+++ app/index.js
@@ -1,11 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-require('index.css');
-//state
+require('./index.css');
+
+//state 
 //lifecycle events
 //UI
 
-class App extends React.Component (
+class App extends React.Component {
     render() {
         return (
             <div>
@@ -13,7 +14,7 @@ class App extends React.Component (
             </div>
         )
     }
-)
+}
 
 
 ReactDOM.render(
